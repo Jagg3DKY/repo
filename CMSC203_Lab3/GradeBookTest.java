8328e29ecb2894739d5aa3b5f0b11e487108e5ef
Jagg3DKY <kyrancalcagno@gmail.com>
2024-10-16 10:41:09 -0400

    Add files via upload

---
+++ CMSC203_Lab3/GradeBookTest.java
@@ -0,0 +1,52 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GradeBookTest {
+	private GradeBook gB1, gB2;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		gB1 = new GradeBook(5);
+		gB2 = new GradeBook(5);
+		gB1.addScore(56.2);
+		gB1.addScore(27.3);
+		gB1.addScore(87.4);
+		gB2.addScore(22.1);
+		gB2.addScore(72.8);
+		gB2.addScore(95.1);
+		gB2.addScore(62.8);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		gB1 = null;
+		gB2 = null;
+	}
+
+	@Test
+	void testAddScore() {
+		assertTrue(gB1.toString().equals("56.2 27.3 87.4"));
+		assertEquals(3, gB1.getScoreSize());
+	}
+
+	@Test
+	void testSum() {
+		double expectedSum = 56.2 + 27.3 + 87.4;
+		assertEquals(expectedSum, gB1.sum(), 0.01);
+	}
+
+	@Test
+	void testMinimum() {
+		double expectedSum = 27.3;
+		assertEquals(expectedSum, gB1.minimum(), 0.01);
+	}
+
+	@Test
+	void testFinalScore() {
+		assertEquals(56.2 + 87.4, gB1.finalScore(), .01);
+	}
+	
+}
